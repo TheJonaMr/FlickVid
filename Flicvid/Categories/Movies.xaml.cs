9b0b6aa9e5053adfaaee65fe42b810efe02e18d7
thejonamr <the.jona.mr@outlook.com>
2016-06-26 18:01:20 +0200

    hello world

---
+++ Flicvid/Categories/Movies.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+//using System.Windows.Shapes;
+using System.IO;
+
+namespace Flicvid.Categories
+{
+    /// <summary>
+    /// Interaction logic for Movies.xaml
+    /// </summary>
+    public partial class Movies : Page
+    {
+        public Movies()
+        {
+            InitializeComponent();
+            LoadMovies();
+        }
+
+        private void LoadMovies()
+        {
+            string Moviepath = @"F:\Torrent\The Boy and The Beast";
+
+            for (int i = 0; i < 11; i++)
+            {
+                //ListViewItem Movie = new ListViewItem();
+
+                //Movie.Name = Path.GetFileName(Moviepath);
+                
+
+                //MovieList.Items.Add(Movie);
+            }
+        }
+    }
+}
---
+++ Flicvid/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flicvid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MoviesButton_Click(object sender, RoutedEventArgs e)
+        {
+            Uri NewSource = new Uri("Categories/Movies.xaml", UriKind.Relative);
+            CategoryFrame.Source = NewSource;
+        }
+
+        private void SeriesButton_Click(object sender, RoutedEventArgs e)
+        {
+            Uri NewSource = new Uri("Categories/Series.xaml", UriKind.Relative);
+            CategoryFrame.Source = NewSource;
+        }
+
+        private void AnimeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Uri NewSource = new Uri("Categories/Anime.xaml", UriKind.Relative);
+            CategoryFrame.Source = NewSource;
+        }
+
+        private void FlicvidButton_Click(object sender, RoutedEventArgs e)
+        {
+            Uri NewSource = new Uri("Categories/Flicvid.xaml", UriKind.Relative);
+            CategoryFrame.Source = NewSource;
+        }
+
+        private void FlickvidMainWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape) this.Close();
+        }
+    }
+}
